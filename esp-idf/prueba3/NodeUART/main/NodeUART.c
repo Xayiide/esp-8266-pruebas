07ca033a53d57b9aa241c8e67da9f4467ce05cf4
Xayiide <wickedcheerio@protonmail.com>
2023-09-05 17:21:13 +0200

    La prueba 3 se deja a medias por líos con la UART jeje

--- esp-idf/prueba3/NodeUART/main/NodeUART.c
+++ esp-idf/prueba3/NodeUART/main/NodeUART.c
@@ -4,7 +4,7 @@
 #include "driver/gpio.h"
 #include "driver/uart.h"
 
-#define LED_OUT 13
+#define LED_OUT 14 // D5
 #define GPIO_OUTPUT_MASK ((1ULL << LED_OUT));
 
 #define BUF_SIZE 1024
@@ -12,27 +12,29 @@
 static void uart_task()
 {
     int            len;
-    uint8_t       *data;
-    uart_config_t  uart_config = {
-        .baud_rate = 9600,
+    uint8_t       *rxdata;
+    uart_config_t  uart0_config = {
+        .baud_rate = 74880,
         .data_bits = UART_DATA_8_BITS,
         .parity    = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
         .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
     };
+    uart_config_t uart1_config = uart0_config;
 
-    uart_param_config(UART_NUM_0, &uart_config);
+    uart_param_config(UART_NUM_0, &uart0_config);
+    uart_param_config(UART_NUM_1, &uart1_config);
     uart_driver_install(UART_NUM_0, BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_driver_install(UART_NUM_1, BUF_SIZE * 2, BUF_SIZE * 2, 0, NULL, 0);
 
-    data = (uint8_t *) malloc(BUF_SIZE);
+    rxdata = (uint8_t *) malloc(BUF_SIZE);
 
     while (1) {
-        len = uart_read_bytes(UART_NUM_0, data, BUF_SIZE, 20/portTICK_RATE_MS);
+        len = uart_read_bytes(UART_NUM_0, rxdata, BUF_SIZE, 20/portTICK_RATE_MS);
         if (len == 1) {
-            if (*data == '1')
-                gpio_set_level(LED_OUT, 1);
-            else if (*data == '0')
-                gpio_set_level(LED_OUT, 0);
+            printf("[+] Recibido el byte 0x%x. Enviándolo por la UART 1\n", *rxdata);
+            uart_tx_chars(UART_NUM_1, (const char *) rxdata, len);
+            uart_write_bytes(UART_NUM_1, (const char *) rxdata, len);
         }
     }
 }
