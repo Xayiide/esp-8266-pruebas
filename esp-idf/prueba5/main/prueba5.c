750eab8761dac8f4b241e0e1373415d4d4f8c87a
Xayiide <wickedcheerio@protonmail.com>
2023-09-12 17:10:38 +0200

    Se limpia la prueba 5

--- esp-idf/prueba5/main/prueba5.c
+++ esp-idf/prueba5/main/prueba5.c
@@ -1,26 +1,24 @@
+#include <stdio.h>
 #include "driver/adc.h"        /* adc_config_t                */
 #include "freertos/FreeRTOS.h" /* portTICK_PERIOD_MS          */
 #include "freertos/task.h"     /* vTaskDelay                  */
 #include "esp_log.h"           /* ESP_LOGI                    */
 
-static const char *TAG = "adc example";
+#define DEC_PLACE_MUL 10000
 
 static void adc_task()
 {
-    int x;
-    uint16_t adc_data[100];
+    uint16_t adc_data;
+    double   voltage;
+    int32_t  aux_v;
 
     while (1) {
-        if (ESP_OK == adc_read(&adc_data[0])) {
-            ESP_LOGI(TAG, "adc read: %d\r\n", adc_data[0]);
-        }
-
-        ESP_LOGI(TAG, "adc read fast:\r\n");
-
-        if (ESP_OK == adc_read_fast(adc_data, 100)) {
-            for (x = 0; x < 100; x++) {
-                printf("%d\n", adc_data[x]);
-            }
+        if (adc_read(&adc_data) == ESP_OK) {
+            voltage = adc_data * (3.3 / 1023);
+            aux_v   = voltage * DEC_PLACE_MUL;
+            printf("[+] adc %d -- ", adc_data);
+            printf("%d.%2u\r\n",
+                    aux_v / DEC_PLACE_MUL, abs(aux_v) * DEC_PLACE_MUL);
         }
 
         vTaskDelay(1000 / portTICK_RATE_MS);
@@ -32,11 +30,9 @@ void app_main()
 {
     adc_config_t adc_config;
 
-    adc_config.mode = ADC_READ_TOUT_MODE;
+    adc_config.mode    = ADC_READ_TOUT_MODE;
     adc_config.clk_div = 8;
     ESP_ERROR_CHECK(adc_init(&adc_config));
 
     xTaskCreate(adc_task, "adc_task", 1024, NULL, 5, NULL);
-
-
 }
